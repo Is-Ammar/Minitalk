5041b82fbaee28482595f464df4fa69e38b4c609
Ismail Ammar <iammar@c2r1p8.1337.ma>
2024-12-20 11:07:45 +0100

    add: utils optimisation

--- utils.c
+++ utils.c
@@ -15,7 +15,6 @@
 int	ft_atoi(const char *str)
 {
 	long int	result;
-	long int	result2;
 	int			sign;
 
 	result = 0;
@@ -31,22 +30,14 @@ int	ft_atoi(const char *str)
 		str++;
 	while (*str >= '0' && *str <= '9')
 	{
-		result2 = result;
 		result = result * 10 + (*str++ - '0');
-		if (result < result2 && sign == -1)
-			return (0);
-		if (result < result2 && sign == 1)
-			return (-1);
 	}
-	return (sign * result);
+	return ((int)sign * result);
 }
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	num;
-
-	if (fd == -1)
-		return ;
+	char c;
 	if (n == -2147483648)
 	{
 		write(fd, "-2147483648", 11);
@@ -57,15 +48,14 @@ void	ft_putnbr_fd(int n, int fd)
 		write(fd, "-", 1);
 		n = -n;
 	}
-	if (n > 9)
+	if (n >= 10)
 	{
 		ft_putnbr_fd(n / 10, fd);
-		num = '0' + n % 10;
-		write(fd, &num, 1);
+		ft_putnbr_fd(n % 10, fd);
 	}
-	if (n < 10)
+	else
 	{
-		num = '0' + n;
-		write(fd, &num, 1);
+		c = n + '0';
+		write(fd, &c, 1);
 	}
 }
